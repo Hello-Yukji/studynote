4deb071ce058f3f8ec6e8ff9275359569491bf6b
Hello-Yukji <dkalrh1530@naver.com>
2024-11-23 10:22:29 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,34 @@
+# print 공부
+
+print (1 + 2)
+print (3 + 5)
+print ("야" + "호")
+print ("육" + str(1)) # str : string 스트링, 문자
+
+ 
+# def 함수 공부
+def plusplus(a, b) :
+    return a + b
+
+def plustest(a, b) :
+    print ("아아아")
+    c = a + b
+    return b
+
+# for 공부 
+# 반복문
+
+
+
+raise()
+import time
+for num in range(10) :
+    print ("[ERROR]시스템을 재시작해주세요..." + str(num))
+    time.sleep(1) # 1초 뒤 
+
+for num in range(5) :
+    print ("[WARNNING] 로켓발싸 " + str(5 - num) +"초 전...")
+    time.sleep(1)
+
+print ("봄브")
+
